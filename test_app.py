710221f60505adac3474ea196f661022d3a3544f
Desmond Onam <desmondonam@gmail.com>
2024-09-05 05:28:22 +0300

    add pytest

--- test_app.py
+++ test_app.py
@@ -1,5 +1,15 @@
+import pytest 
 from app import add
 
-def test_add():
-    assert add(2,3) == 5
-    assert add(-1, 1) == 0
+def test_add_positive_numbers():
+    assert add(2,5) == 7
+
+def test_add_negative_numbers():
+    assert add(-3, -5) == -8
+
+def test_add_positive_and_negative_numbers():
+    assert add(2, -3) == -1
+
+def test_add_zero():
+    assert add(0, 5) == 5
+    assert add(0, 0) == 0
